874324645ae436d4ac53ffdeb6f4957e7dff219f
Zebaztian Jochumzen <143087602+zebaztianjochumzen@users.noreply.github.com>
2023-11-15 22:00:19 +0100

    Fixat totalsumma, Fixat summa per vara

--- Projekt/Mainprogram.py
+++ Projekt/Mainprogram.py
@@ -21,7 +21,9 @@ class Database:
 
     def __init__(self):
         self.goods = []
-        self.products1 = {} #Detta är en dictionary som lagrar key: Code och value: Name 
+        self.products = {} #Detta är en dictionary som lagrar key: Code och value: Name 
+        self.products_price = {}
+        self.products_amount = {}
     
     def read_goods(self):
         file = open("goods.txt", "r", encoding="utf-8")
@@ -32,7 +34,9 @@ class Database:
             amount = int(file.readline().strip())
             new_goods = Goods(code,name,price,amount)
             self.goods.append(new_goods)
-            self.products1.update({code: name}) # Denna biten är ny och lägger till attributen code och name. Code = key och Name = Value
+            self.products.update({code : name}) # Denna biten är ny och lägger till attributen code och name. Code = key och Name = Value
+            self.products_price.update({code : price})
+            self.products_amount.update({code : amount})
             code = file.readline().strip()
         file.close()
 
@@ -48,7 +52,9 @@ class Database:
     def show_goods(self):
         for products in self.goods:
             print(products)
-        print(self.products1)
+        print(self.products)
+        print(self.products_price)
+        print(self.products_amount)
     
     def new_goods(self):
         code = int(input("PLU CODE: "))
@@ -57,37 +63,59 @@ class Database:
         amount = int(input("AMOUNT "))
         new = Goods(code, name, price, amount)
         self.goods.append(new)
-        self.products1.update({code : name}) # Denna biten är ny och lägger till attributen code och name. Code = key och Name = Value
+        self.products.update({code : name,})
+        self.products_price.update({code : price})
+        self.products_amount.update({code : amount}) # Denna biten är ny och lägger till attributen code och name. Code = key och Name = Value
     
     def cashier_mode(self):
         print("You have now entered the cashier mode:")
         print("Just write out the product codes you want to add and the quantity")
         print("To exit and get a receipt finish with (#)")
-        cart = []
+        self.cart = []
+        self.price = []
+        total = 0
         
         while True:
             user_input = (input().split(" "))
             if user_input[0] == "#":
                     break
             if all(element.isdigit() for element in user_input):
-                if user_input[0] in self.products1:
-                    cart.append(self.products1[user_input[0]])
+                if user_input[0] in self.products:
+                    self.cart.append(self.products[user_input[0]])
                     if len(user_input) is 1:
-                        cart.append(1)
+                        self.cart.append(1)
                     else:
-                        cart.append(user_input[1])
+                        self.cart.append(user_input[1])
                 else:
                     print("That item does not exist")
                     continue
             else:
                 print("Invalid Input")
-            
+            """
+            If statementet nedan kollar om user_input är kopplat till ett pris, alltså att varan man skriver in är ett pris
+            Det som händer då är att den multiplicerar den andra delen av user_input och sen lägger till det i en lista. 
+            Detta gör alltså att man kan se totalpriset baserat för varje vara baserat på hur många varor man köper.
 
-        print(cart)
+            """
+            if user_input[0] in self.products_price:
+                self.price.append((self.products_price[user_input[0]] * int(user_input[1])))
+            else:
+                print("The product that you wrote does not have an assigned price\nPlease try again")
 
-                
+        """
+        Det denna den for loopen under gör att den tar listan self.price och sen så tar den och summerar alla positionerna med ett
+        specefikt värde med varandra så att man får en total summa. 
+        """
         
+        for i in self.price:
+            total += int(i)
+            
+        print("The item in your cart is:",self.cart)
+        print(self.price)
+        print("Total", total)
 
+        
+        
         """while True:
             input1 = input().split(" ")
             cart.append(input1)
@@ -115,25 +143,14 @@ class Database:
             else: 
                 print(total)
                 continue
-    """            
-# class Cashier:
-    """Planen med den är funktionen är att vi ska vara i cashier mode, vilket innebär att vi ska kunna 
-        uppfylla de kraven på ett användarvänligt program, där "kassören" ska kunna skriva in PLU koden 
-        från databasen och sedan skriva in antalet. För att sedan kunna avsluta med #.
-        """
-    
-    
-            
-
-    
-
-    """def sum_products():
+              
+    def sum_products():
         Planen med denna funktionen är att den ska beräkna det totala priset beroende på antalet produkter som kassören skriver in. Hur vet jag inte än...
         
     def write_out_receipt():
       
         Har absolut ingen aning om hur jag ska framställa kvittot än lol.
-        """
+    """
 
 def menu():
     print("-------------------------------")
@@ -151,20 +168,23 @@ def main():
     database.read_goods()
     cashier = database
     option = menu()
-    while menu != "Q":
+    while True != "Q":
         if option == "A":
             database.new_goods()
             database.save_goods()
+            break
         elif option == "S":
             print("\n")
             print("\n" "The products in the database is:")
             database.show_goods()
+            break
         if option == "C":
             database.read_goods()
             cashier.cashier_mode()
+            break
+        elif option == "Q":
+            print("Bye!")
+            quit()
         option =menu()
-    print("Bye!")
-
 if __name__ == "__main__":
     main()
- 
