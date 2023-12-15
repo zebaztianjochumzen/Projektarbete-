301f0d4fc7a92835071d1cd1181e3474525c206a
Zebaztian Jochumzen <zebaztian.jochumzen@gmail.com>
2023-12-15 10:00:01 +0100

    Projekt klart

--- Projekt/errorhandeling.py
+++ Projekt/errorhandeling.py
@@ -1,18 +1,9 @@
 def get_valid_input(prompt):
-    """A loop that checks whether user input can be converted to int, ends when successful"""
+    """A loop that checks whether user input can be converted to int and checks so its positive, ends when successful"""
     while True:
         try:
             user_input = int(input(prompt))
-            return user_input
-        except ValueError:
-            print("Invalid input. Please enter a valid integer.")
-            
-def a_number_but_not_zero(prompt):
-    """A loop that checks whether user input can be converted to int, ends when successful"""
-    while True:
-        try:
-            user_input = int(input(prompt))
-            if user_input != 0:
+            if user_input >= 0:
                 return user_input
         except ValueError:
             print("Invalid input. Please enter a valid integer.")
--- Projekt/main.py
+++ Projekt/main.py
@@ -19,7 +19,7 @@ class Cashier:
 
     def __init__(self):
         """This creates the different lists and dictionaries to enable the cashier function to have its funtionality"""
-        self.goods = []
+        #self.goods = []
         self.products = {}
         self.products_price = {}
         self.products_amount = {}
@@ -33,7 +33,7 @@ class Cashier:
             price = int(file.readline().strip())
             amount = int(file.readline().strip())
             new_goods = Good(code,name,price,amount)
-            self.goods.append(new_goods)
+            #self.goods.append(new_goods)
             self.products.update({code : name}) 
             self.products_price.update({code : price})
             self.products_amount.update({code : amount})
@@ -43,13 +43,11 @@ class Cashier:
     
     def cashier_mode(self):
         """ This function is the main part of the program, its the user friendly part where you can actually write in the products you want to add.
-            It also contains error handeling, so you can not write in odd inputs.
-        """
+            It also contains error handeling, so you can not write in odd inputs."""
         print("You have now entered the cashier mode:")
-        print("Just write out the product codes you want to add and the quantity")
+        print("Just write out the productcodes you want to add and the quantity")
         print("To exit and get a receipt finish with (#)")
         self.cart = []
-        
         while True:
             user_input = (input().split(" "))
             if user_input[0] == "#":
@@ -80,34 +78,48 @@ class Cashier:
         print("This is the items in the basket")
         for item in self.cart:
             print(f"{item}\n")
-
     
+            
+        
+
     def edit_receipt(self):
-        """This function enables the user to change the amount before the receipt prints out, this also enables the user to delete unasked products"""
+        """This function enables the user to change the amount before the receipt prints out, this also enables the user to delete unasked products, it also checks if the amount 
+        the user types in exceeds the amount in the database."""
         while True:
             end_question = input("Do you want to edit the cart? ").lower() 
-            if end_question == "yes":
+            if end_question == "yes" and len(self.cart)>0:
                 edit_line = (er.get_valid_input("Which line do you want to edit? ") -1)
                 if 0 <= edit_line < len(self.cart):
+                    print("The line you are edeting", self.cart[edit_line])
                     i, j = 0, 0
                     while j == 0:
-                        if edit_line < len(self.cart):
+                        if edit_line < len(self.cart) or edit_line > len(self.cart):
                             edit_question = input("What do you want to edit? ").lower()
-                            if edit_question == "amount":
+                            if edit_question == "a":
                                 while i == 0:
                                     new_amount = er.get_valid_input("Enter the new amount: ")
                                     self.cart[edit_line][1] = new_amount
-                                    if int(new_amount) == 0:
-                                        del self.cart[edit_line]
-                                        print("Line removed.")
-                                        i += 1
-                                        j += 1
-                                        break
-                                    else:
-                                        print("Edit successful")
-                                        i += 1
-                                        j += 1
-                                        break
+                                    for key, value in self.products.items():
+                                        if self.cart[edit_line][0] == value:
+                                            if key in self.products_amount:
+                                                if new_amount > self.products_amount[key]:
+                                                    print("Remember that the new amount cant exceed the database amount!")
+                                                    i = 0
+                                        
+                                                elif int(new_amount) == 0:
+                                                        del self.cart[edit_line]
+                                                        print("Line removed.")
+                                                        i += 1
+                                                        j += 1
+                                                        break
+                                                else:
+                                                    new_price = (self.cart[edit_line][2] * int(new_amount))
+                                                    self.cart[edit_line][3] = new_price
+                                                    print("Edit successful")
+                                                    print(self.cart[edit_line])
+                                                    i += 1
+                                                    j += 1
+                                                    break
                             else:
                                 print(f"Editing {edit_question} is not supported.")
                                 j = 0
@@ -116,8 +128,10 @@ class Cashier:
                             j = 0
                 else: 
                     print("Item not found, please try again")
+                    
             elif end_question == "no":
                 break
+                
         print("Printing receipt.....")
 
     def write_out_receipt(self):
@@ -147,7 +161,9 @@ def menu():
 
 def main():
     """This is the main function which controlls all the other functions"""
-    print("\n","Welcome")
+    print("\n"+"         "+"Welcome")
+    print("This is a cashier software")
+    print("Please choose a option below")
     print("-------------------------------")
     database = Cashier()
     database.read_goods()
